f56ec1296a7ceb620ad117c83167117be48571d7
Stark Wolf <cryptostarkwolf@gmail.com>
2020-11-01 17:09:07 -0800

    Updates

---
+++ precache-manifest.72158573cc804cdd1dc5548280ce0a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6653520fb9482c6ca0c39899c2dc031e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ca07c98775a76610e16",
+    "url": "./static/css/2.b5a80ab8.chunk.css"
+  },
+  {
+    "revision": "34e30aac94e974230bbd",
+    "url": "./static/css/main.0d3a6a61.chunk.css"
+  },
+  {
+    "revision": "2ca07c98775a76610e16",
+    "url": "./static/js/2.5443549d.chunk.js"
+  },
+  {
+    "revision": "6ff87620e4f720e979dad85ffac87bda",
+    "url": "./static/js/2.5443549d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e30aac94e974230bbd",
+    "url": "./static/js/main.4b32e62c.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
